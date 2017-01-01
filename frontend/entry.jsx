0bfdb1ca6c5fb7d2d435124589e991dba61e0640
Igor Dmitriev <igor.s.dmitriev@gmail.com>
2016-12-31 23:23:23 -0800

    Init commit, setup react app

---
+++ frontend/entry.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Synthesizer from './synthesizer';
+
+document.addEventListener("DOMContentLoaded", function(){
+  ReactDOM.render(<Synthesizer />, document.getElementById('root'));
+});
---
+++ frontend/synthesizer.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+class Synthesizer extends React.Component {
+  constructor () {
+    super();
+  }
+
+  render () {
+    return (
+      <div>
+        <h1>Synthesizer</h1>
+      </div>
+    );
+  }
+}
+
+export default Synthesizer;
